8a89aee987179d1bbce9396be3aac9d8f821722f
shashank <shashankkumar2023@gmail.com>
2021-05-20 02:36:37 +0530

    make it better

---
+++ index.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors'
+
+import dotenv from 'dotenv';
+import postRoutes from './routes/posts.js';
+
+const app= express();
+dotenv.config();
+// this ensures all the request of post would go through the
+// localhost:5000/posts/..... not localhost:5000/
+
+// body-parser extracts the entire body portion of an 
+// incoming request stream and exposes it on req.body.
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+
+app.use('/posts',postRoutes);
+// http://wwww.mongodb.com/cloud/atlas
+
+
+const PORT = process.env.PORT || 5000;
+
+// This verifies if db is connected successfully or not, if yes then app 
+// listen on the given port no.or else it will throw an error
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+  // to avoid warning in console
+mongoose.set('useFindAndModify', false);   
