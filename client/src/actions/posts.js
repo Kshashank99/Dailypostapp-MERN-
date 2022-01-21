0dae4eaec7d6166a76d8b5c03fdc8e5ae09f421d
shashank <shashankkumar2023@gmail.com>
2022-01-21 20:24:54 +0530

    implemented google 0 auth authentication and set up JWT auth backend

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,62 +1,70 @@
-import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE ,DISLIKE} from '../constants/actionTypes';
+import {
+	FETCH_ALL,
+	CREATE,
+	UPDATE,
+	DELETE,
+	LIKE,
+	DISLIKE
+} from "../constants/actionTypes";
 
-import * as api from '../api/index.js';
+import * as api from "../api/index.js";
 
 export const getPosts = () => async (dispatch) => {
-  try {
-    const { data } = await api.fetchPosts();
+	try {
+		const { data } = await api.fetchPosts();
 
-    dispatch({ type: FETCH_ALL, payload: data });
-  } catch (error) {
-    console.log(error.message);
-  }
+		dispatch({ type: FETCH_ALL, payload: data });
+	} catch (error) {
+		console.log(error.message);
+	}
 };
 
 export const createPost = (post) => async (dispatch) => {
-  try {
-    const { data } = await api.createPost(post);
+	try {
+		const { data } = await api.createPost(post);
+		console.log(data);
 
-    dispatch({ type: CREATE, payload: data });
-  } catch (error) {
-    console.log(error.message);
-  }
+		dispatch({ type: CREATE, payload: data });
+	} catch (error) {
+		console.log(error.message);
+	}
 };
 
 export const updatePost = (id, post) => async (dispatch) => {
-  try {
-    const { data } = await api.updatePost(id, post);
+	try {
+		const { data } = await api.updatePost(id, post);
 
-    dispatch({ type: UPDATE, payload: data });
-  } catch (error) {
-    console.log(error.message);
-  }
+		dispatch({ type: UPDATE, payload: data });
+	} catch (error) {
+		console.log(error.message);
+	}
 };
 
 export const likePost = (id) => async (dispatch) => {
-  try {
-    const { data } = await api.likePost(id);
+	try {
+		const { data } = await api.likePost(id);
 
-    dispatch({ type: LIKE, payload: data });
-  } catch (error) {
-    console.log(error.message);
-  }
+		dispatch({ type: LIKE, payload: data });
+	} catch (error) {
+		console.log(error.message);
+	}
 };
 export const DislikePost = (id) => async (dispatch) => {
-  try {
-    const { data } = await api.DislikePost(id);
+	try {
+		const { data } = await api.DislikePost(id);
 
-    dispatch({ type: DISLIKE, payload: data });
-  } catch (error) {
-    console.log(error.message);
-  }
+		dispatch({ type: DISLIKE, payload: data });
+	} catch (error) {
+		console.log(error.message);
+	}
 };
 
 export const deletePost = (id) => async (dispatch) => {
-  try {
-    await api.deletePost(id);
+	try {
+		await api.deletePost(id);
 
-    dispatch({ type: DELETE, payload: id });
-  } catch (error) {
-    console.log(error.message);
-  }
+		dispatch({ type: DELETE, payload: id });
+	} catch (error) {
+		console.log(error.message);
+	}
 };
