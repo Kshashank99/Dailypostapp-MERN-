1114b8a39dc60ff10755f3e836e13a76834b7d76
shashank <shashankkumar2023@gmail.com>
2022-02-23 05:31:04 +0530

    fixed bugs

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -3,11 +3,13 @@ import axios from "axios";
 const API = axios.create({ baseURL: "http://localhost:5000" });
 
 API.interceptors.request.use((req) => {
-  if (localStorage.getItem('profile')) {
-    req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
-  }
+	if (localStorage.getItem("profile")) {
+		req.headers.Authorization = `Bearer ${
+			JSON.parse(localStorage.getItem("profile")).token
+		}`;
+	}
 
-  return req;
+	return req;
 });
 
 export const fetchPosts = () => API.get("/posts");
